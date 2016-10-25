a38b00c9a7d50b54d0784df29d2a24829f02c59d
slee <slee@slee-crash.lab.tripwire.com>
2016-10-25 12:01:45 -0700

    I think i figured out the balance thing

--- phone.js
+++ phone.js
@@ -1,6 +1,5 @@
 var phonePrice = 25
 const TAX = .09
-var initialBalance = 412
 var numPhones = 0
 var numAccessories = 0
 var accessoryPrice = 6
@@ -25,28 +24,30 @@ console.log(phoneCostFinal);
 console.log(accessoryCostFinal);
 
 //calculate balance
-//(function (cost) { balance = balance - cost})();
-function getBalance (balance, cost) {
-    balance = balance - cost;
+function getBalance (balance) {
+    var currentBalance;
+    currentBalance = balance;
+    return currentBalance;
 }
 
 //initialize Balance
-var curBalance = getBalance(412, 0);
-
+var curBalance = getBalance(400);
+console.log(curBalance);
 
 do{
 //purchase phones
     if(curBalance >= threshold + phoneCostFinal){  
     numPhones++;
     purchaseTotal += phoneCostFinal;
-    curBalance = getBalance(balance, phoneCostFinal);
+    curBalance = getBalance(curBalance - phoneCostFinal);
     console.log("You added a phone to your order. Phone cost is $" + phoneCostFinal + "  Your remaining balance is  $" + curBalance.toFixed(2));
     }
-    // if(threshold + accessoryCost()){
-    // numAccessories++;
-    // (function () { balance = balance - accessoryCost()})();
-    // console.log("You added an accessory to your order. Accessory cost is $" + accessoryCostFinal + " Your remaining balance is  $" + balance.toFixed(2));
-    // }
+    if(curBalance >= threshold + accessoryCost()){
+    numAccessories++;
+    purchaseTotal += accessoryCostFinal;
+    curBalance = getBalance(curBalance - accessoryCostFinal);
+    console.log("You added an accessory to your order. Accessory cost is $" + accessoryCostFinal + " Your remaining balance is  $" + curBalance.toFixed(2));
+    }
 }
 while(curBalance > threshold + accessoryCost())
 
