48cba564efc20c60284309749b004f5cee101aa7
Susan Lee <reachsusanlee@gmail.com>
2016-12-11 13:58:45 -0800

    added map and markers for sneaker bikes

--- finalproject/app.js
+++ finalproject/app.js
@@ -1,9 +1,18 @@
 var selection = document.getElementById("select");
-
 var dataButton = document.getElementById("getdata");
 var mapButton = document.getElementById("getmap");
-var div = document.getElementById("results");
+var results = document.getElementById("results");
 var mapArea = document.getElementById("map")
+
+var areaMap = L.map('map').fitBounds([
+    [45.6077682, -122.9945375],
+    [45.4289472, -122.4139835]
+]);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+}).addTo(areaMap);
 
 dataButton.addEventListener("click", function() {
 
@@ -21,28 +30,29 @@ dataButton.addEventListener("click", function() {
         var data = JSON.parse(xhr.responseText);
         var htmlString = "";
         console.log(selection);
+        htmlString += '<h3>Total Free Bikes available ' + data.data.bikes.length+ '</h3>'
 
         if(selection.value === "Any"){
-            htmlString += '<h3>Total Free Bikes available ' + data.data.bikes.length+ '</h3>'
+            
             for(var i=0; i< data.data.bikes.length; i++){
                 htmlString += '<p>' + JSON.stringify(data.data.bikes[i]['bike_id']) + '</p>'
                 htmlString += '<p>' + JSON.stringify(data.data.bikes[i]['name']) + '</p>'
             
             }
-            div.innerHTML = htmlString + '<div><button id="getMap">Map It?</button></div>';
+            results.innerHTML = htmlString + '<div><button id="getMap">Map It?</button></div>';
             mapButton.hidden = "false"
         };
         
         if(selection.value === "Sneaker"){
-            htmlString += '<h3>Total Free Bikes available ' + data.data.bikes.length+ '</h3>'
             for(var i=0; i< data.data.bikes.length; i++){
                 if(data.data.bikes[i]['name'].search("BIKETOWN") === -1){
                 htmlString += '<p>' + data.data.bikes[i]['bike_id'] + '</p>'
                 htmlString += '<p>' + data.data.bikes[i]['name'] + '</p>'
+                var marker = L.marker([data.data.bikes[i]['lat'], data.data.bikes[i]['lon']]).bindPopup(data.data.bikes[i]['name']).addTo(areaMap);
                 }  
             
             }
-            div.innerHTML = htmlString + '<div><button id="getMap">Map It?</button></div>';
+            results.innerHTML = htmlString + '<div><button id="getMap">Map It?</button></div>';
             mapButton.hidden = "false"
         };
       
@@ -51,16 +61,3 @@ dataButton.addEventListener("click", function() {
 
 
 });
-
-// mapButton.addEventListener("click", function() {
-//     var url = "http://api.openstreetmap.org/api/0.6/map?bbox=-122.7417,45.4876,-122.5381,45.5722"
-//     var xhr = new XMLHttpRequest();
-
-//     xhr.addEventListener("load", callback);
-//     xhr.open("GET", url);
-//     xhr.send();
-
-//     function callback(){
-//         var map = XML
-//     }
-// });
